e22f6685dca6842b8544234a6077bb52291c82cb
Guilherme-R-Santos <guilhermersantos99@outlook.com>
2025-06-25 23:38:56 -0300

    Changes to be committed: 	modified:   css/hero.css 	modified:   css/style.css 	deleted:    img/BannerHeroPrecisao(incompleto).xcf 	modified:   index.html 	modified:   js/index.js 	new file:   js/simulador.js 	new file:   simulador.html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,73 @@
+// Adiciona funcionalidade de scroll suave para o menu "Início"
+const inicio = document.getElementById('menu-inicio');
+inicio.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+// Adiciona funcionalidade de scroll suave para os itens do menu
+const sobre = document.getElementById('menu-sobre');
+sobre.addEventListener('click', function() {
+    const section = document.getElementById('sobre-nos');
+    const headerHeight = document.getElementById('header').offsetHeight;
+    const sectionPosition = section.offsetTop - headerHeight;
+    
+    window.scrollTo({
+        top: sectionPosition,
+        behavior: 'smooth'
+    });
+});
+
+const services = document.getElementById('menu-services');
+services.addEventListener('click', function() {
+    const section = document.getElementById('services');
+    const headerHeight = document.getElementById('header').offsetHeight;
+    const sectionPosition = section.offsetTop - headerHeight;
+    
+    window.scrollTo({
+        top: sectionPosition,
+        behavior: 'smooth'
+    });
+});
+
+const benefits = document.getElementById('menu-benefits');
+benefits.addEventListener('click', function() {
+    const section = document.getElementById('benefits');
+    const headerHeight = document.getElementById('header').offsetHeight;
+    const sectionPosition = section.offsetTop - headerHeight;
+    
+    window.scrollTo({
+        top: sectionPosition,
+        behavior: 'smooth'
+    });
+});
+
+const contato = document.getElementById('menu-contato');
+contato.addEventListener('click', function() {  
+    const section = document.getElementById('contato');
+    const headerHeight = document.getElementById('header').offsetHeight;
+    const sectionPosition = section.offsetTop - headerHeight;
+    
+    window.scrollTo({
+        top: sectionPosition,
+        behavior: 'smooth'
+    });
+});
+
+// Adiciona funcionalidade de scroll suave para o botão "Saiba Mais" do banner hero
+const btnHero = document.getElementById('btn-hero');
+btnHero.addEventListener('click', function() {
+    const section = document.getElementById('services');
+    const headerHeight = document.getElementById('header').offsetHeight;
+    const sectionPosition = section.offsetTop - headerHeight;
+    
+    window.scrollTo({
+        top: sectionPosition,
+        behavior: 'smooth'
+    });
+});
 
 // Evita envio reset da página pós envio do formulário
 const form = document.getElementById('form');
@@ -13,38 +83,28 @@ function enviaFormulario() {
     alert(`Obrigado, ${nome}! Sua mensagem foi enviada com sucesso. Em breve entraremos em contato.`);
 }
 
-// Adiciona funcionalidade de scroll suave para o menu "Início"
-const inicio = document.getElementById('menu-inicio');
-inicio.addEventListener('click', function() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
+function irParaSimulador() {
+    const pagAtual = window.location.pathname;
+    if (pagAtual === '/simulador.html') {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    } else {
+        window.location.href = '/simulador.html';
+    }
+}
 
-// Adiciona funcionalidade de scroll suave para os outros itens do menu
-const sobre = document.getElementById('menu-sobre');
-sobre.addEventListener('click', function() {
-    const section = document.getElementById('sobre-nos');
-    section.scrollIntoView({
-        behavior: 'smooth'
+const btnSimulador = document.getElementsByClassName('btn-simulador');
+if (btnSimulador.length > 0) {
+    btnSimulador[0].addEventListener('click', function() {
+        irParaSimulador();
     });
-});
+}
 
-// Adiciona funcionalidade de scroll suave para o menu "Serviços"
-const servicos = document.getElementById('menu-servicos');
-servicos.addEventListener('click', function() {
-    const section = document.getElementById('servicos');
-    section.scrollIntoView({
-        behavior: 'smooth'
+const btnSimuladorFormulario = document.getElementById('btn-simulador-formulario');
+if (btnSimuladorFormulario) {
+    btnSimuladorFormulario.addEventListener('click', function() {
+        irParaSimulador();
     });
-});
-
-// Adiciona funcionalidade de scroll suave para o menu "Serviços"
-const contato = document.getElementById('menu-contato');
-contato.addEventListener('click', function() {  
-    const section = document.getElementById('contato');
-    section.scrollIntoView({
-        behavior: 'smooth'
-    });
-});
+}
